684430f7d92728b030ad4b0869f85611ba8adf62
sneaky-potato <rajivkamal.im421@gmail.com>
2022-03-22 12:48:19 +0530

    update scraper to include full list of glossary

--- scraper.py
+++ scraper.py
@@ -4,20 +4,47 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.headless = True
 
+
+webList = [
+    'https://whatis.techtarget.com/glossary/Linux',
+    'https://whatis.techtarget.com/glossary/Software-Applications',
+    'https://whatis.techtarget.com/glossary/Storage-and-Data-Management',
+    'https://whatis.techtarget.com/glossary/Web-services-SOA',
+    'https://whatis.techtarget.com/glossary/Operating-Systems',
+]
+
 browser = webdriver.Firefox(options=options, executable_path="./drivers/geckodriver")
+browser.get('https://whatis.techtarget.com/glossary/Web-services-SOA')
 
-browser.get('https://whatis.techtarget.com/glossary/Linux')
-
-beeglist = browser.find_element_by_id("definitions-listing")
-defs = beeglist.find_elements_by_tag_name("li")
+nPages = browser.find_element_by_class_name("browse-alpha-bridge-nav")
+nPages = nPages.find_elements_by_tag_name("li")
+print(len(nPages))
 
 f = open("output.txt", "w")
 
-print(len(defs))
+pageList = []
 
-for definition in defs:
-    f.write(definition.text)
-    f.write("\n")
+for page in nPages:
+    anchor = page.find_element_by_tag_name("a")
+    link = anchor.get_attribute("href")
+    pageList.append(link)
+
+browser.quit()
+
+for pageLink in pageList: 
+
+    nbrowser = webdriver.Firefox(options=options, executable_path="./drivers/geckodriver")
+    nbrowser.get(pageLink)
+
+    beeglist = nbrowser.find_element_by_id("definitions-listing")
+    defs = beeglist.find_elements_by_tag_name("li")
+
+    print(len(defs))
+
+    for definition in defs:
+        f.write(definition.text)
+        f.write("\n")
+    
+    nbrowser.quit()
 
 f.close()
-browser.quit()
